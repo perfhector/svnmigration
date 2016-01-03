fded47d8b0513974e35200cf20485300e72960ff
Perfhector <perfhector@perfhector.info>
2016-01-03 11:07:34 +0100

    adding test list

--- svnmigration/src/test/groovy/AddUsersToRepositoriesXmlTest.groovy
+++ svnmigration/src/test/groovy/AddUsersToRepositoriesXmlTest.groovy
@@ -15,23 +15,30 @@ class AddUsersToRepositoriesXmlTest extends GroovyTestCase {
     public void testWriteXML(){
 
         //def reposMap =  ['repo1':['false','user','READ']]
-        def reposMap =['reponame':['user1':'WRITE', 'user2':'WRITE', 'user3':'WRITE']]
+        def reposMap =['repo1':['user1':'WRITE', 'user2':'WRITE', 'user3':'WRITE']]
         
         def xmlContent="<?xml version=\"1.0\" ?><repositorydb><repositories><repository><name>repo1</name><permissions><permission><groupPermission>false</groupPermission></permission><name>moi</name><type>OWNER</type></permissions></repository></repositories></repositorydb>"
         
         AddUsersToRepositoriesXml clazz  = new AddUsersToRepositoriesXml();
-        //String result = clazz.writeXML(reposMap, xmlContent);
-        def result = clazz.wXML(reposMap, xmlContent);
+        def result = clazz.writeXML(reposMap, xmlContent);
         
         
         println result
-        //println XmlUtil.serialize(result)
+
                 
         
         assertTrue(result.indexOf("moi")>0)
         assertTrue(result.indexOf("user1" )>0)
         assertTrue(result.indexOf("user2" )>0)
         assertTrue(result.indexOf("user3" )>0)
-        //assertFalse(result.indexOf("lui")>0)
+
+    }
+    
+    public void testListRepoFromXml(){
+        
+        def xmlContent="<?xml version=\"1.0\" ?><repositorydb><repositories><repository><name>repo1</name><permissions><permission><groupPermission>false</groupPermission></permission><name>moi</name><type>OWNER</type></permissions></repository></repositories></repositorydb>"
+        AddUsersToRepositoriesXml clazz  = new AddUsersToRepositoriesXml();
+        clazz.listRepoFromXML(xmlContent);
+        assertTrue(true)
     }
 }
