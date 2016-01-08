a0d8570f62c84f3e5ecd0f594d805a47443b8094
Cedric Levasseur <cedric.levasseur@cegedim.com>
2016-01-08 17:05:07 +0100

    better unit test, reformatting, changing the xml root tagname

--- svnmigration/src/main/groovy/AddUsersToRepositoriesXml.groovy
+++ svnmigration/src/main/groovy/AddUsersToRepositoriesXml.groovy
@@ -22,29 +22,20 @@ public class AddUsersToRepositoriesXml{
         def xmlParsed = new XmlSlurper(false, true).parseText( xmlFileContent )
         println "reposMap----»"+reposMap
         xmlParsed.repositories.repository.each(){
-            String reponame=it.name
-            println "treating xmlrepo:$reponame"
-            //println "get repo from name("+it.name+"-»"+reposMap.find(reponame)
-            println "get repo from name("+it.name+"--»"+reposMap[reponame]
-            def login=reposMap[reponame]
-       
-        reposMap[reponame].each { k,v ->
-                println "{[$reponame]k=$k,v=$v}"
-                def fragmentToAdd =   { 
-                    permission {
-                        name(k)
-                        groupPermission("false")
-                        type(v)
+            String reponame=it.name //converting from a node to a String
+            reposMap[reponame].each { k,v ->
+                    println "inserting xml for $reponame : $k $v"
+                    def fragmentToAdd =   { 
+                        permission {
+                            name(k)
+                            groupPermission("false")
+                            type(v)
+                        }
                     }
+                    def nodeRepo = xmlParsed.repositories.repository.find{ it.name == reponame }
+                    nodeRepo.permissions.appendNode( fragmentToAdd )
                 }
-                println "inserting xml"
-                def nodeRepo = xmlParsed.repositories.repository.find{ it.name == reponame }
-                //xmlParsed.repositories.repository.permissions.appendNode( fragmentToAdd )
-                nodeRepo.permissions.appendNode( fragmentToAdd )
-            }
-        }  
-        return XmlUtil.serialize(xmlParsed)
-        
-    }
-    
+            }  
+            return XmlUtil.serialize(xmlParsed)        
+    }    
 }
--- svnmigration/src/test/groovy/AddUsersToRepositoriesXmlTest.groovy
+++ svnmigration/src/test/groovy/AddUsersToRepositoriesXmlTest.groovy
@@ -17,21 +17,26 @@ class AddUsersToRepositoriesXmlTest extends GroovyTestCase {
         //def reposMap =  ['repo1':['false','user','READ']]
         def reposMap =['repo1':['user1':'WRITE', 'user2':'WRITE', 'user3':'WRITE']]
         
-        def xmlContent="<?xml version=\"1.0\" ?><repositorydb><repositories><repository><name>repo1</name><permissions><permission><groupPermission>false</groupPermission></permission><name>moi</name><type>OWNER</type></permissions></repository><repository><name>repo2</name><permissions><permission><groupPermission>false</groupPermission></permission><name>moi</name><type>OWNER</type></permissions></repository></repositories></repositorydb>"
+        def xmlContent="<?xml version=\"1.0\" ?><repository-db><repositories><repository><name>repo1</name><permissions><permission><groupPermission>false</groupPermission></permission><name>moi</name><type>OWNER</type></permissions></repository><repository><name>repo2</name><permissions><permission><groupPermission>false</groupPermission></permission><name>moi</name><type>OWNER</type></permissions></repository></repositories></repository-db>"
         
         AddUsersToRepositoriesXml clazz  = new AddUsersToRepositoriesXml();
         def result = clazz.writeXML(reposMap, xmlContent);
-        
-        
+
         println result
-
-                
-        
-        assertTrue(result.indexOf("moi")>0)
-        assertTrue(result.indexOf("user1" )>0)
-        assertTrue(result.indexOf("user2" )>0)
-        assertTrue(result.indexOf("user3" )>0)
-
+       
+        def imoi=result.indexOf("moi")
+        def i1=result.indexOf("user1")
+        def i2=result.indexOf("user2")
+        def i3=result.indexOf("user3")
+        def ir2=result.indexOf("repo2")
+        assertTrue(imoi>0)
+        assertTrue(i1>0)
+        assertTrue(i2>0)
+        assertTrue(i3>0)
+        assertTrue(ir2>0)
+        assertTrue(i1<ir2)
+        assertTrue(i2<ir2)
+        assertTrue(i3<ir2)
     }
     
     public void testListRepoFromXml(){
